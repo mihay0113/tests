1aefcd3c6b6ee773e82ef09e5db4911094af25e7
Michael <83721291+mihay0113@users.noreply.github.com>
2021-08-17 10:21:16 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import './App.scss';
+
+export const App: FunctionComponent = () => (
+  <div className="starter">
+    React starter pack
+  </div>
+);
